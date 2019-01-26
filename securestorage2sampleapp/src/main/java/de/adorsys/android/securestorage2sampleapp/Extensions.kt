fa2628642129a15095321daba05163476618cd72
Drilon Reçica <drilonrecica.dev@gmail.com>
2019-01-26 14:41:29 +0100

    Added new extension method for getting Spanned text

--- securestorage2sampleapp/src/main/java/de/adorsys/android/securestorage2sampleapp/Extensions.kt
+++ securestorage2sampleapp/src/main/java/de/adorsys/android/securestorage2sampleapp/Extensions.kt
@@ -2,10 +2,22 @@ package de.adorsys.android.securestorage2sampleapp
 
 import android.app.Activity
 import android.content.Context
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 fun hideKeyboard(context: Context, view: View) {
     val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun getSpannedText(text: String): Spanned {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        Html.fromHtml(text, Html.FROM_HTML_MODE_COMPACT)
+    } else {
+        @Suppress("DEPRECATION")
+        Html.fromHtml(text)
+    }
 }
