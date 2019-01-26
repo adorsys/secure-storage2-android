f6a1451fa843ba317745578b54e4a447332a2d33
Drilon Reçica <drilonrecica.dev@gmail.com>
2019-01-26 15:03:35 +0100

    Created function in Extensions to show custom Snackbar with user defined messages and length

--- securestorage2sampleapp/src/main/java/de/adorsys/android/securestorage2sampleapp/Extensions.kt
+++ securestorage2sampleapp/src/main/java/de/adorsys/android/securestorage2sampleapp/Extensions.kt
@@ -3,10 +3,13 @@ package de.adorsys.android.securestorage2sampleapp
 import android.app.Activity
 import android.content.Context
 import android.os.Build
+import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
 import android.text.Html
 import android.text.Spanned
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 
 fun hideKeyboard(context: Context, view: View) {
     val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -20,4 +23,17 @@ fun getSpannedText(text: String): Spanned {
         @Suppress("DEPRECATION")
         Html.fromHtml(text)
     }
+}
+
+fun showSnackbar(rootView: View, message: String, durationShort: Boolean) {
+    val duration = if (durationShort) {
+        Snackbar.LENGTH_SHORT
+    } else {
+        Snackbar.LENGTH_LONG
+    }
+
+    val snackbar = Snackbar.make(rootView, message, duration)
+    snackbar.view.setBackgroundColor(ContextCompat.getColor(rootView.context, R.color.colorPrimary))
+    snackbar.view.findViewById<TextView>(android.support.design.R.id.snackbar_text).setTextColor(ContextCompat.getColor(rootView.context, android.R.color.white))
+    snackbar.show()
 }
