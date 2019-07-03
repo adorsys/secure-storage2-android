ccde29735cfdc96eb77df593b800cbee0a63db02
drilonrecica <drilonrecica.dev@gmail.com>
2019-07-03 20:54:25 +0200

    Setup fresh project

---
+++ app/src/main/java/de/adorsys/android/securestorage2sampleapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package de.adorsys.android.securestorage2sampleapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
