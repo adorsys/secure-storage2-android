fb5a5e320093e741d99b18a2d14e2c81e6f83a44
drilonrecica <drilonrecica.dev@gmail.com>
2019-07-04 23:25:57 +0200

    Created SecureStorageConfig enum
    The SecureStorageConfig enum is used to initialize the library and set necessary config values

--- securestorage2/src/main/java/de/adorsys/android/securestorage2/SecureStorageConfig.kt
+++ securestorage2/src/main/java/de/adorsys/android/securestorage2/SecureStorageConfig.kt
@@ -1,15 +1,36 @@
 package de.adorsys.android.securestorage2
 
 import android.content.Context
+import de.adorsys.android.securestorage2.internal.KeyStoreTool
 
 enum class SecureStorageConfig {
     INSTANCE;
 
     internal lateinit var SHARED_PREFERENCES_NAME: String
     internal lateinit var ENCRYPTION_KEY_ALIAS: String
+    internal lateinit var X500PRINCIPAL: String
+    internal var CAN_USE_LIBRARY = false
+    internal var ASYNC_OPERATION = false
 
-    fun initializeSecureStorageConfig(context: Context, ecnryptionKeyAlias: String) {
+    fun initializeSecureStorageConfig(
+        context: Context,
+        encryptionKeyAlias: String,
+        x500Principal: String,
+        useOnlyWithHardwareSupport: Boolean,
+        workWithDataAsynchronously: Boolean
+    ) {
         SHARED_PREFERENCES_NAME = context.packageName + ".SecureStorage2"
-        ENCRYPTION_KEY_ALIAS = ecnryptionKeyAlias
+        ENCRYPTION_KEY_ALIAS = encryptionKeyAlias
+        X500PRINCIPAL = x500Principal
+        ASYNC_OPERATION = workWithDataAsynchronously
+
+        CAN_USE_LIBRARY = if (useOnlyWithHardwareSupport) {
+            KeyStoreTool.deviceHasSecureHardwareSupport(context)
+        } else {
+            true
+        }
     }
+
+    // this is how a x500principal must look:
+//    "CN=App Name, O=Company Name, C=Country"
 }
